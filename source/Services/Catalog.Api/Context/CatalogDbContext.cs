31219528af9c3eb3c3308ebba54f6a7579bf5d61
mmraihan <mm_raihan@live.com>
2023-12-04 22:43:48 +0600

    16. Product entity, Data layer with connection string, configured MongoDb.

---
+++ source/Services/Catalog.Api/Context/CatalogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using MongoRepo.Context;
+
+namespace Catalog.Api.Context
+{
+    public class CatalogDbContext : ApplicationDbContext
+    {
+        //Get Data from the appsetting.json
+        static IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+
+
+        static string connectionString = configuration.GetConnectionString("Catalog.Api");
+        static string databaseName = configuration.GetValue<string>("DatabaseName");
+        public CatalogDbContext() : base(connectionString, databaseName)
+        {
+        }
+    }
+}
