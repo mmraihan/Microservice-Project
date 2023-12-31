64b3284cd824bf285bc2994705cf12e5bc135423
mmraihan <mm_raihan@live.com>
2023-12-31 20:07:59 +0600

    Updated style enetity

--- source/Services/Basket.Api/Entities/Style.cs
+++ source/Services/Basket.Api/Entities/Style.cs
@@ -16,5 +16,7 @@
         public int Id { get; set; }
         public string StyleName { get; set; }
         public string UserName { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
     }
 }
