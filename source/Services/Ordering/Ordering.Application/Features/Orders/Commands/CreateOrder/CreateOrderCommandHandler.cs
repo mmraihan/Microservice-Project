7eeb27d4c2645be0cf50fea72a4bd6f2aac4616b
mmraihan <mm_raihan@live.com>
2024-12-21 00:11:39 +0600

    82.Clean Architecture: Application layer Consumed infrastructure with Order Confirmation Email.

--- source/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ source/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using MediatR;
+using Ordering.Application.Contacts.Infrastructure;
 using Ordering.Application.Contacts.Persistence;
+using Ordering.Application.Models;
 using Ordering.Domain.EntityModels;
 using System;
 using System.Collections.Generic;
@@ -14,17 +16,36 @@ namespace Ordering.Application.Features.Orders.Commands.CreateOrder
     {
         IOrderRepository _orderRepository;
         IMapper _mapper;
+        IEmailService _emailService;
 
-        public CreateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper)
+        public CreateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, IEmailService emailService)
         {
             _orderRepository = orderRepository;
             _mapper = mapper;
+            _emailService = emailService;
         }
 
         public async Task<bool> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
         {
             var order = _mapper.Map<Order>(request);
-            return await _orderRepository.AddAsync(order);
+            order.CreatedBy = "1";
+            order.CreatedDate = DateTime.Now;
+            bool isOrderPlaced = await _orderRepository.AddAsync(order);
+            if (isOrderPlaced)
+            {
+                EmailMessage email = new EmailMessage();
+                email.Subject = "Your Order Has Been Successfully Placed";
+                email.To = order.UserName;
+                email.Body = $"Dear {order.FirstName} {order.LastName},<br/><br/>" +
+                             $"We are thrilled to inform you that your order (Order ID: #{order.Id}) has been successfully placed. " +
+                             $"We will notify you as soon as it is on its way.<br/><br/>" +
+                             "Thank you for shopping with Mubasshir Raihan Team.<br/><br/>" +
+                             "Best regards,<br/>" +
+                             "The Mubasshir Raihan Team";
+                await _emailService.SendEmailAsync(email);
+            }
+
+            return isOrderPlaced;
         }
     }
 }
