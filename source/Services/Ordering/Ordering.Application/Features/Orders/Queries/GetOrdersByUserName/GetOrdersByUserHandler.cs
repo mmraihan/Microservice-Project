8dc1155078da22a42d18ffd2ce942fe1fbf0f34a
mmraihan <mm_raihan@live.com>
2024-12-20 12:57:19 +0600

    77. Clean Architecture: Application layer consumed with Features by Queries with MediatR.

---
+++ source/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersByUserName/GetOrdersByUserHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contacts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersByUserName
+{
+    public class GetOrdersByUserHandler : IRequestHandler<GetOrdersByUserQuery, List<OrderVm>>
+    {
+        IOrderRepository _orderRepository;
+        IMapper _mapper;
+        public GetOrdersByUserHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<OrderVm>> Handle(GetOrdersByUserQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrderVm>>(orders);
+        }
+    }
+}
---
+++ source/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersByUserName;
+using Ordering.Domain.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Order,OrderVm>().ReverseMap();
+        }
+    }
+}
