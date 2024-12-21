99a24b8733d50ffb96e6e711ce73311f1f5da8df
mmraihan <mm_raihan@live.com>
2024-12-22 00:54:37 +0600

    84-86.Clean Architecture: Infrastructure layer Consumed by repository and Seed data, DbContext.

---
+++ source/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>().HasData(
+              new Order()
+              {
+                  Id = 1,
+                  UserName = "mmraihan99@gmail.com",
+                  FirstName = "Mubasshir",
+                  LastName = "Raihan",
+                  EmailAddress = "mmraihan99@gmail.com",
+                  Address = "Chattogram",
+                  TotalPrice = 100,
+                  City = "Chattogram"
+                  //CVV = "Test",
+                  //CardName = "Test",
+                  //CardNumber = "Test",
+                  //Expiraton = "Test",
+                  //PaymentMethod = 1,
+                  //CreatedBy = "Test",
+                  //CreatedDate = DateTime.Now,
+                  //PhoneNumber = "0160000000",
+                  //State = "Test",
+                  //ZipCode = "Test"
+              }
+             );
+
+        }
+    }
+}
---
+++ source/Services/Ordering/Ordering.Infrastructure/Repository/OrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using EF.Core.Repository.Repository;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contacts.Persistence;
+using Ordering.Domain.EntityModels;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repository
+{
+    public class OrderRepository : CommonRepository<Order>, IOrderRepository
+    {
+        private readonly OrderDbContext _dbContext;
+        public OrderRepository(OrderDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders.Where(c => c.UserName.ToLower() == userName.ToLower()).ToListAsync();
+            return orderList;
+        }
+    }
+}
