8ccad13265cb3fd8bbb6c191a2f7be55f766bf41
mmraihan <mm_raihan@live.com>
2024-12-22 23:09:05 +0600

    87-88.Clean Architecture: Infrastructure layer Consumed by Email service.

---
+++ source/Services/Ordering/Ordering.Application/Models/EmailCredential.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Models
+{
+    public static class EmailCredential
+    {
+        public static string EmailAddress = "mmraihan99@gmail.com";
+        public static string Password = "cfod jpbl crxl aedr";
+    }
+}
